8b7dd99b336a2926597245cb057b3a8f65c76e35
MaZderMind <github@mazdermind.de>
2014-10-27 15:09:11 +0100

    link LQ and HD-MP4s in the download-section currently only Hackover13 has its LQ-Files registered in the media database but this may or may not change, in the near future, at the latest on the 31C3

--- lib/models/recording.rb
+++ lib/models/recording.rb
@@ -5,7 +5,7 @@ class Recording < ActiveRecord::Base
 
   scope :downloaded, -> { where(state: 'downloaded') }
   scope :audio, -> { where(mime_type: ['audio/ogg', 'audio/mpeg']) }
-  scope :video, -> { where(mime_type: ['video/mp4', 'video/ogg', 'video/webm']) }
+  scope :video, -> { where(mime_type: ['video/mp4', 'vnd.voc/h264-lq', 'vnd.voc/h264-hd', 'video/ogg', 'video/webm']) }
 
   def url
     File.join self.event.conference.recordings_url, self.folder || '', self.filename
@@ -13,6 +13,17 @@ class Recording < ActiveRecord::Base
 
   def torrent_url
     url + '.torrent'
+  end
+
+  def filetype
+    case mime_type
+    when 'vnd.voc/h264-lq'
+      'MP4 (LQ)'
+    when 'vnd.voc/h264-hd'
+      'MP4 (HD)'
+    else
+      mime_type.split('/')[1]
+    end
   end
 
 end
